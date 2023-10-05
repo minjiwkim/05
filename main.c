c75d081b3772896903cffd289128a673b7787e54
minjiwkim <minji5489235790@gmail.com>
2023-10-05 16:39:34 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,19 +4,28 @@
 int main(int argc, char *argv[])
 {           
     
-    int sum = 0;
+    int answer = 32;
     int num;
-    int i;
+    int trial = 0;
     
-    printf("input a number:");
-    scanf("%d", &num); 
-    
-    for (i=0 ; i<=num ; i++)
+    do
     {
-        sum = sum + i;
-    }
+     //1. input
+     printf("Guess a number :");
+     scanf("%d", &num); 
+     
+     //2. decision (low/high) (wrong answer)
+     if ( num > answer )
+     printf("high!\n");
+     else if ( num < answer )
+     printf("low!\n");
     
-    printf("result is %i\n", sum);
+    trial = trial + 1;
+    }
+    while( num != answer ); //3. loop condition : answer is equal to num
+    
+    printf("Congratulation! trials:%d\n", trial);
+
 
   system("PAUSE");	
   return 0;
